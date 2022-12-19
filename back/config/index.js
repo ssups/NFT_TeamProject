4670500cc60778b74eba6e272e7c79a733db2e48
ssups <shalstlqruki@gmail.com>
2022-12-19 15:23:30 +0900

    init

---
+++ back/config/index.js
@@ -0,0 +1,10 @@
+const config = {
+  dev: {
+    password: process.env.DATABASE_PASSWORD,
+    database: "nft_project",
+    host: "127.0.0.1",
+    username: "root",
+    dialect: "mysql",
+    logging: false,
+  },
+};
