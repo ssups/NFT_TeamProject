20d5b611203276fb75b4c08d43a984b65c2e9823
mydreamis-18 <hitoryok@naver.com>
2022-12-19 16:15:15 +0900

    [docs] 작업 일지 작성
    초기 폴더 구조 생성

--- back/controller/app.js
+++ back/controller/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const address = "http://localhost:3000";
 const { sequelize } = require("../models");
 
+// 시퀄라이즈 테이블 생성 후에 DB 작업을 요청할 예정
 let isSequelizeDone = false;
 
 // axios 데이터 통신 시 필요
@@ -12,7 +13,6 @@ app.use(express.json());
 
 app.use(cors({ origin: address }));
 
-//
 sequelize.sync({ force: true }).then(() => {
   console.log("시퀄라이즈");
   isSequelizeDone = true;
