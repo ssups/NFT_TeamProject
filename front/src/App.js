41da52f677be5c20e6beb5ecb86226eebd2b9a71
ssups <shalstlqruki@gmail.com>
2022-12-20 09:45:42 +0900

    testPage

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,35 @@
-import React from 'react'
-import "./App.css"
-import Brand from './components/Brand/Brand'
-import Footer from './components/Footer/Footer'
-import Header from './components/header/Header'
-import Info from './components/Info/Info'
-import TopFold from './components/TopFold/TopFold'
-import TrendingNft from './components/TrendingNft/TrendingNft'
+import React from "react";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import "./App.css";
+import Brand from "./components/Brand/Brand";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/header/Header";
+import Info from "./components/Info/Info";
+import TopFold from "./components/TopFold/TopFold";
+import TrendingNft from "./components/TrendingNft/TrendingNft";
+import Minseop from "./components/minseop/minseop";
 
 const App = () => {
   return (
     <div>
-      <Header/>
-      <TopFold/>
-      <Brand/>
-      <TrendingNft/>
-      <Info/>
-      <Footer/>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <TopFold />
+              <Brand />
+              <TrendingNft />
+              <Info />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/minseop" element={<Minseop />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ front/src/components/minseop/minseop.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const minseop = () => {
+  return <div>minseop 하위하위하위</div>;
+};
+
+export default minseop;
