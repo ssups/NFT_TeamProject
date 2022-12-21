f88701e4925e771296c5f17f051baa7a8196180e
ssups <shalstlqruki@gmail.com>
2022-12-21 16:36:08 +0900

    forMerge

--- front/src/components/minseop/seopMy.js
+++ front/src/components/minseop/seopMy.js
@@ -3,6 +3,7 @@ import useWeb3 from "../../hooks/useWeb3";
 import TestTokenContract from "../../contracts_seop/TestToken.json";
 
 const SeopMy = () => {
+  const [account, web3, balance] = useWeb3();
   return <div>SeopMy</div>;
 };
 
