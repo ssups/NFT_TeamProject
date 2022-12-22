9a6fac874e56b3632145a75ae5c003a0d0d2d101
rnr9928 <rnr99281@gmail.com>
2022-12-22 11:16:55 +0900

    merge

--- blockChain/contracts/MintingNft.sol
+++ blockChain/contracts/MintingNft.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.17;
 
 // 확장성 고려
-// import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-// import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 // remix 경로 수정
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 // NFT 발행에 대한 컨트랙트
 contract MintingNft is ERC721Enumerable, Ownable {
@@ -15,7 +15,7 @@ contract MintingNft is ERC721Enumerable, Ownable {
     // 토큰 발행량 제한
     uint immutable private _mintingLimit;
 
-    // 랜덤의 토큰을 발행 받기 위한 고정 비용 (단위 확인 필요..)
+    // 랜덤의 토큰을 발행 받기 위한 고정 비용 (wei 단위)
     uint private _mintingPrice;
 
     // 백 서버 주소
@@ -30,28 +30,35 @@ contract MintingNft is ERC721Enumerable, Ownable {
     // 배포자 EOA 계정 접근
     // owner() public
 
-    // 토큰 민팅 이벤트, 검색을 위해 indexed 사용
-    event MintingToken(address indexed owner, uint indexed tokenId);
-
     // json 파일 이름을 tokenId로 활용 예정
-    function mintToken(address _owner, uint _tokenId) external payable {
+    function mintToken(address _tokenOwner, uint _tokenId) external payable {
 
-        // wei 단위인지 확인 필요..
+        // wei 단위
         require(_mintingPrice <= msg.value, "insufficient money to mint");
 
         // 현재 발행량 확인 후 토큰 발행
         require(totalSupply() < _mintingLimit, "exceeding the maximum issuance");
 
-        emit MintingToken(_owner, _tokenId);
-
         payable(owner()).transfer(_mintingPrice);
 
         // ERC721Enumerable 상태 변수 업데이트
-        // 해당 함수 안의 조건문 때문에 값을 두 번 추가..
-        _beforeTokenTransfer(address(0), _owner, _tokenId, 1);
+        // 소유자 없는 토큰을 추가로 하나 더 발행하는 이슈 발생..
+        _beforeTokenTransfer(address(0), _tokenOwner, _tokenId, 1);
 
-        // ERC721 상태 변수 업데이트 (토큰 id 중복 확인 기능 포함)
-        _mint(_owner, _tokenId);
+        // event indexed 키워드 확인..
+        // ERC721 상태 변수 업데이트 (토큰 id 중복 확인 및 event 기능 내장)
+        _mint(_tokenOwner, _tokenId);
+    }
+
+    // 토큰 소유자 변경 함수
+    function transferToken(address _from, address _to, uint _tokenId) external {
+        
+        emit Transfer(_from, _to, _tokenId);
+
+        transferFrom(_from, _to, _tokenId);
+
+        _beforeTokenTransfer(_from, address(0), _tokenId, 1);
+        _beforeTokenTransfer(address(0), _to, _tokenId, 1);
     }
 
     // _allTokens 모든 토큰 id 배열
@@ -59,13 +66,31 @@ contract MintingNft is ERC721Enumerable, Ownable {
     // _ownedTokens 소유자 주소 => (보유 토큰 인덱스 => 토큰 id)
     // _ownedTokensIndex 토큰 id => 소유자 기준 보유 토큰 인덱스
 
+    // wei 단위
+    function setMintingPrice(uint _price) external onlyOwner {
+
+        require(_mintingPrice != _price, "same as previous price");
+
+        _mintingPrice = _price;
+    }
+
+    function setDefaultPath(string memory defaultPath_) external onlyOwner {
+        _defaultPath = defaultPath_;
+    }
+    
+    // 토큰에 대한 정보가 담긴 JSON 파일에 접근할 수 있도록 파일 경로를 반환하는 함수
+    function tokenURI(uint _tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(_defaultPath, _tokenId));
+    }
+
     // 보유 토큰 조회 함수
+    // 마이페이지에서 판매 허용 미설정 시 최초 1회 setApprovalForAll(address TrandingNft, true) 함수 실행 예정
     function getMyTokenIds(address _owner) external view returns (uint[] memory) {
         
         // 보유 토큰 수
         uint count = balanceOf(_owner);
         
-        require(count > 0);
+        require(count > 0, "no token");
 
         uint[] memory tokenIds = new uint[](count);
         
@@ -77,29 +102,23 @@ contract MintingNft is ERC721Enumerable, Ownable {
 
         return tokenIds;
     }
-
-    // 단위 확인 필요..
-    function setMintingPrice(uint _price) external onlyOwner {
-        _mintingPrice = _price;
-    }
-
-    function setDefaultPath(string memory defaultPath_) external onlyOwner {
-        _defaultPath = defaultPath_;
-    }
+    
+    // 해당 함수를 호출하는 계정을 반환하는 함수
+    // function msgSender() external view returns (address) {
+    //     return _msgSender();
+    // }
 
     function mintingPrice() external view returns (uint) {
         return _mintingPrice;
     }
 
-    // 사용할 이미지 경로 : defaultPath()/tokenId
-    function defaultPath() public view returns (string memory) {
+    // 사용할 이미지 경로 : defaultPath() / tokenId
+    function defaultPath() external view returns (string memory) {
         return _defaultPath;
     }
 
-    // 토큰에 대한 정보가 담긴 JSON 파일에 접근할 수 있도록 파일 경로를 반환하는 함수
-    function tokenURI(uint _tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked(_defaultPath, _tokenId));
-    }
+    // 함수 코드 순서
+    // set => view => public => external => private
 
     // totalSupply() 모든 토큰의 개수
     // tokenByIndex(_index) 모든 토큰의 해당 인덱스의 토큰 id 조회 함수
