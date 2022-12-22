8b9e60881f7959bdc34b0378a8847e6beac093cc
rnr9928 <rnr99281@gmail.com>
2022-12-22 09:24:24 +0900

    img

--- front/src/components/minseop/seopMy.js
+++ front/src/components/minseop/seopMy.js
@@ -1,9 +1,9 @@
-import React from "react";
-import useWeb3 from "../../hooks/useWeb3";
-import TestTokenContract from "../../contracts_seop/TestToken.json";
+// import React from "react";
+// import useWeb3 from "../../hooks/useWeb3";
+// import TestTokenContract from "../../contracts_seop/TestToken.json";
 
-const SeopMy = () => {
-  return <div>SeopMy</div>;
-};
+// const SeopMy = () => {
+//   return <div>SeopMy</div>;
+// };
 
-export default SeopMy;
+// export default SeopMy;
