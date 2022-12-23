4b425d43a03967f5533d87fdf46ee83d70455831
ssups <shalstlqruki@gmail.com>
2022-12-23 18:02:43 +0900

    아몰랑

--- front/src/components/Header2/Header2.js
+++ front/src/components/Header2/Header2.js
@@ -1,89 +1,96 @@
-import React, { useEffect, useRef } from 'react'
-import { NavLink , Link } from 'react-router-dom'
-import "../../styles/header2.css"
+import React, { useContext, useEffect, useRef } from "react";
+import { NavLink, Link } from "react-router-dom";
+import "../../styles/header2.css";
 
-import { Container } from 'reactstrap'
-
-
+import { Container } from "reactstrap";
+import { Context } from "../Layout/Layout";
 //헤더 링크
 const Nav_Link = [
-    {
-        display : "Home",
-        url: "/home"
-    },
-    {
-        display : "Shop",
-        url: "/shop"
-    },
-    {
-        display : "Create",
-        url: "/create"
-    },
-
-]
+  {
+    display: "Home",
+    url: "/home",
+  },
+  {
+    display: "Shop",
+    url: "/shop",
+  },
+  {
+    display: "Create",
+    url: "/create",
+  },
+];
 
 const Header2 = () => {
+  const { web3 } = useContext(Context);
+  // 지갑연결 함수
+  async function connectWallet() {
+    await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+  }
 
-    // 헤더바 고정
-    const headerRef = useRef(null)
+  // 헤더바 고정
+  const headerRef = useRef(null);
 
-    useEffect(() =>{ console.log('hihihi')
-        window.addEventListener('scroll',()=>{
-            if(document.body.scrollTop > 80 || document.documentElement.scrollTop >80){
-                headerRef.current.classList.add('header2_stick')
-            }
-            else{
-                headerRef.current.classList.remove('header2_stick')
-            }
-        })
+  useEffect(() => {
+    console.log("hihihi");
+    window.addEventListener("scroll", () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add("header2_stick");
+      } else {
+        headerRef.current.classList.remove("header2_stick");
+      }
+    });
 
-        return ()=>{
-            window.removeEventListener('scroll',()=>{})
-        }
-    },[])
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
   return (
     // 헤더
-    <header className='header2' ref={headerRef}> 
-        <Container>
-            {/* 로고 */}
-        <div className='navigation'>
-            <div className='logo'>   
-                <h2 className='d-flex gap-2 align-items-center'>
-                    <span> 
-                    <i className="ri-fire-fill"></i>              
-                    </span>
-                    SSAN DE NFT
-                </h2>
-            </div>
+    <header className="header2" ref={headerRef}>
+      <Container>
+        {/* 로고 */}
+        <div className="navigation">
+          <div className="logo">
+            <h2 className="d-flex gap-2 align-items-center">
+              <span>
+                <i className="ri-fire-fill"></i>
+              </span>
+              SSAN DE NFT
+            </h2>
+          </div>
 
-            <div className='nav_menu'>
-                <ul className='nav_list'>
-                    {Nav_Link.map((item,index) =>(
-                        <li className='nav_item' key={index}>
-                        <NavLink to={item.url} className={navClass=> 
-                        navClass.isActive?'active':''}>{item.display}</NavLink>
-                    </li> 
-                    ))}
-                   
-                </ul>
-            </div>
+          <div className="nav_menu">
+            <ul className="nav_list">
+              {Nav_Link.map((item, index) => (
+                <li className="nav_item" key={index}>
+                  <NavLink
+                    to={item.url}
+                    className={navClass => (navClass.isActive ? "active" : "")}
+                  >
+                    {item.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-                            {/* 지갑버튼 */}
-            <div className='nav_right d-flex align-items-center gap-5'>
-                <button className='btn1'>                   
-                    <Link  className='d-flex gap-2 align-items-center'>
-                    <span>
-                    <i className="ri-wallet-3-fill"></i>
-                        </span>
-                         지갑 연동
-                        </Link>
-                       
-                </button>
-            </div>
+          {/* 지갑버튼 */}
+          <div className="nav_right d-flex align-items-center gap-5">
+            <button className="btn1" onClick={connectWallet}>
+              <Link className="d-flex gap-2 align-items-center">
+                <span>
+                  <i className="ri-wallet-3-fill"></i>
+                </span>
+                지갑 연동
+              </Link>
+            </button>
+          </div>
         </div>
-        </Container>
+      </Container>
     </header>
-  )
-}
+  );
+};
 
-export default Header2
+export default Header2;
--- front/src/components/Layout/Layout.js
+++ front/src/components/Layout/Layout.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import Routers from "../../routes/Routers"
-import Header2 from '../Header2/Header2'
-
+import React, { useEffect, useState, createContext } from "react";
+import Routers from "../../routes/Routers";
+import Header2 from "../Header2/Header2";
+import useWeb3 from "../../hooks/useWeb3";
+import useSsandeContracts from "../../hooks/useSsandeContracts";
 const Layout = () => {
+  const [web3, account, balance] = useWeb3();
+  const [tokenContract, tradeContract] = useSsandeContracts();
+
   return (
     <>
-    {/* 전체페이지에 헤더 적용 */}
-    <Header2/>
-    <div>
-        <Routers/>
-    </div>
+      {/* 전체페이지에 헤더 적용 */}
+      <Context.Provider value={{ web3, account, balance }}>
+        <Header2 />
+        <div>
+          <Routers />
+        </div>
+      </Context.Provider>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export const Context = createContext();
+export default Layout;
