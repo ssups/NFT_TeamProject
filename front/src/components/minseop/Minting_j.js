3cb33063d31d2d908dbe5443a98b2239a8c88f95
mydreamis-18 <hitoryok@naver.com>
2022-12-23 14:48:13 +0900

    [feat] 배포자의 CA 잔액 전송 받는 기능 구현 중
    - Minting_j.js
    - updateCaBalance()
    - withDrawAll()

--- front/src/components/minseop/Minting_j.js
+++ front/src/components/minseop/Minting_j.js
@@ -12,7 +12,7 @@ const Minting = () => {
   const [owner, setOwner] = useState();
   const [mintPrice, setMintPrice] = useState();
   const [maxSupply, setMaxSupply] = useState();
-
+  const [caBalance, setCaBalance] = useState();
   const [isMintOn, setIsMintOn] = useState(false);
   const [currentSupply, setCurrentSupply] = useState();
   const [maxAmountPerMint, setMaxAmountPerMint] = useState();
@@ -21,64 +21,6 @@ const Minting = () => {
   const mintQuantityRef = useRef();
 
   const testTokenInstance = useContract(TestTokenContract);
-
-  // ==========================================useEffect==========================================
-  // =============================================================================================
-
-  // 컨트랙트 인스턴스
-  useEffect(() => {
-    //
-    if (!testTokenInstance) return;
-
-    // event callback..
-    // 민팅 시 총 발행량을 업데이트 하는 이벤트
-    testTokenInstance.events.MintTestToken((err, data) => {
-      setCurrentSupply(data.returnValues.totalSupply);
-    });
-
-    // 가격 변경 시 가격과 민팅 여부를 업데이트 하는 이벤트
-    testTokenInstance.events.SetMintOn((err, data) => {
-      //
-      const price = data.returnValues.mintPrice;
-      const priceToEth = web3.utils.fromWei(price, "ether");
-
-      setMintPrice(priceToEth);
-      setIsMintOn(data.returnValues.isMintOn);
-    });
-
-    (async () => {
-      //
-      const owner = await testTokenInstance.methods.owner().call();
-      const isMintOn = await testTokenInstance.methods.isMintOn().call();
-      const maxSupply = await testTokenInstance.methods.maxSupply().call();
-      const maxAmountPerMint = await testTokenInstance.methods.maxAmountPerMint().call();
-
-      setOwner(owner);
-      setIsMintOn(isMintOn);
-      setMaxSupply(maxSupply);
-      setMaxAmountPerMint(maxAmountPerMint * 1);
-    })();
-  }, [testTokenInstance]);
-
-  //
-  // 민팅 시작
-  useEffect(() => {
-    //
-    (async () => {
-      //
-      if (!isMintOn || !testTokenInstance) return;
-
-      const totalSupply = await testTokenInstance.methods.totalSupply().call();
-      const mintPrice = await testTokenInstance.methods.mintPrice().call();
-      const priceToEth = web3.utils.fromWei(mintPrice, "ether");
-
-      setCurrentSupply(totalSupply);
-      setMintPrice(priceToEth);
-
-      // tokenURI() 테스트..
-      //
-    })();
-  }, [isMintOn, testTokenInstance]);
 
   // ==========================================functions==========================================
   // =============================================================================================
@@ -114,10 +56,10 @@ const Minting = () => {
       return;
     }
 
-    if (quantity > remainingSupply) {
-      alert("이 토큰에 설정된 최대 발행량을 초과합니다.");
-      return;
-    }
+    // if (quantity > remainingSupply) {
+    //   alert("이 토큰에 설정된 최대 발행량을 초과합니다.");
+    //   return;
+    // }
 
     if (balance < totalPayment) {
       alert("잔액이 부족합니다.");
@@ -131,7 +73,94 @@ const Minting = () => {
     } catch (err) {
       alert(err.message);
     }
+
+    await testTokenInstance.methods
+      .mintTestToken(quantity)
+      .send({ from: account, value: totalPayment })
+      .then((res) => {
+        console.log(res);
+        console.log("test");
+      })
+      .catch((err) => console.log(err.message));
+
+    // 민팅 성공 시
+    await updateCaBalance();
   }
+
+  async function updateCaBalance() {
+    //
+    const _caBalance = await testTokenInstance.methods.getCaBalance().call();
+    setCaBalance(_caBalance);
+  }
+
+  async function withDrawAll() {
+    //
+    // payable..
+    await testTokenInstance.methods.withdrawAll().send({ value: caBalance });
+  }
+
+  // ==========================================useEffect==========================================
+  // =============================================================================================
+
+  // 컨트랙트 인스턴스
+  useEffect(() => {
+    //
+    if (!testTokenInstance) return;
+
+    // event callback..
+    // 민팅 시 총 발행량을 업데이트 하는 이벤트
+    testTokenInstance.events.MintTestToken((err, data) => {
+      setCurrentSupply(data.returnValues.totalSupply);
+    });
+
+    // 가격 변경 시 가격과 민팅 여부를 업데이트 하는 이벤트
+    testTokenInstance.events.SetMintOn((err, data) => {
+      //
+      const price = data.returnValues.mintPrice;
+      const priceToEth = web3.utils.fromWei(price, "ether");
+
+      setMintPrice(priceToEth);
+      setIsMintOn(data.returnValues.isMintOn);
+    });
+
+    (async () => {
+      //
+      // 최초 1회성
+      const owner = await testTokenInstance.methods.owner().call();
+      const isMintOn = await testTokenInstance.methods.isMintOn().call();
+      const maxSupply = await testTokenInstance.methods.maxSupply().call();
+      const maxAmountPerMint = await testTokenInstance.methods.maxAmountPerMint().call();
+
+      console.log(maxSupply, typeof maxSupply, maxAmountPerMint, typeof maxAmountPerMint);
+
+      setOwner(owner);
+      setIsMintOn(isMintOn);
+      setMaxSupply(maxSupply);
+      setMaxAmountPerMint(maxAmountPerMint * 1);
+
+      await updateCaBalance();
+    })();
+  }, [testTokenInstance]);
+
+  //
+  // 민팅 시작
+  useEffect(() => {
+    //
+    (async () => {
+      //
+      if (!isMintOn || !testTokenInstance) return;
+
+      const totalSupply = await testTokenInstance.methods.totalSupply().call();
+      const mintPrice = await testTokenInstance.methods.mintPrice().call();
+      const priceToEth = web3.utils.fromWei(mintPrice, "ether");
+
+      setCurrentSupply(totalSupply);
+      setMintPrice(priceToEth);
+
+      // tokenURI() 테스트..
+      //
+    })();
+  }, [isMintOn, testTokenInstance]);
 
   // ===========================================returns===========================================
   // =============================================================================================
@@ -144,11 +173,17 @@ const Minting = () => {
     <div>
       {/* 배포자일 경우 */}
       {account?.toLowerCase() === owner?.toLowerCase() && (
-        <div>
-          {/* ref.. */}
-          <input className="minting-price" type="number" ref={priceRef} placeholder="ether 단위" />
-          <button onClick={() => setMintOn(priceRef.current.value)}>민팅 가격 설정</button>
-        </div>
+        <>
+          <div>
+            {/* ref.. */}
+            <input className="minting-price" type="number" ref={priceRef} placeholder="ether 단위" />
+            <button onClick={() => setMintOn(priceRef.current.value)}>민팅 가격 설정</button>
+          </div>
+          <div>
+            <span>현재 이 토큰 컨트랙트의 잔액 : {caBalance}</span>
+            <button onClick={withDrawAll}>내 계좌로 전액 전송하기</button>
+          </div>
+        </>
       )}
 
       <div>{isMintOn ? "민팅 가격 : " + mintPrice + " ether" : "민팅 시작 전"}</div>
@@ -159,10 +194,10 @@ const Minting = () => {
 
       {isMintOn && (
         <div>
-          <input className="minting-quantity" type="text" ref={mintQuantityRef} disabled={currentSupply === maxSupply} />
-          <button onClick={() => mintTestToken(mintQuantityRef.current.value * 1)} disabled={currentSupply === maxSupply}>
-            Mint
-          </button>
+          {/* <input className="minting-quantity" type="text" ref={mintQuantityRef} disabled={currentSupply === maxSupply} />
+          <button onClick={() => mintTestToken(mintQuantityRef.current.value * 1)} disabled={currentSupply === maxSupply}> */}
+          <input className="minting-quantity" type="text" ref={mintQuantityRef} />
+          <button onClick={() => mintTestToken(mintQuantityRef.current.value * 1)}>Mint</button>
         </div>
       )}
     </div>
--- front/src/components/minseop/NftInfo_j.js
+++ front/src/components/minseop/NftInfo_j.js
@@ -6,23 +6,35 @@ const NftInfo = ({ tokenId, nftURI }) => {
   const [jsonData, setJsonData] = useState();
   const [attributes, setAttributes] = useState();
 
+  // ==========================================useEffect==========================================
+  // =============================================================================================
+
   useEffect(() => {
+    //
     (async () => {
+      //
+      // "http://localhost:4000/1.json"
       const jsonData = await axios.get(`${nftURI}.json`);
       setAttributes(jsonData.data.attributes);
       setJsonData(jsonData.data);
     })();
   }, []);
 
+  // ===========================================returns===========================================
+  // =============================================================================================
+
   return (
     <div style={{ marginRight: "10px" }}>
       <div>토큰아이디: {tokenId}</div>
       {jsonData && (
         <>
-          <div>name:{jsonData.name}</div>
+          <div>name : {jsonData.name}</div>
+
+          {/* "http://localhost:4000/images/2.png" */}
           <img style={{ width: "200px", height: "200px" }} src={jsonData.image} alt="" />
+
           {attributes.map((attribute) => {
-            return <div key={attribute.trait_type}>{attribute.trait_type + ":" + attribute.value}</div>;
+            return <div key={attribute.trait_type}>{attribute.trait_type + " : " + attribute.value}</div>;
           })}
         </>
       )}
